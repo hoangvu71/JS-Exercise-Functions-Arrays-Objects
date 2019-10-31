83ce232e4a76f0c825f1228445856eb3b3551141
hoangvu ong(devin) <hoangvu@hoangvus-MacBook-Air.local>
2019-10-31 09:18:42 -0700

    finished the stretch

--- index.js
+++ index.js
@@ -258,8 +258,12 @@ const argTimesTwo = (num) => {return num * 2}; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
+function carMaker(num) {
   /* code here */
+  return {
+    odometer: num,
+    drive(distance) {return this.odometer = this.odometer + distance}
+  }
 }
 
 /// ////// END OF CHALLENGE /////////
